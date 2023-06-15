76b85d73155c82bafaf9265f1e4f42d1744e2147
XIANZHI Ye DTSI/SI <CZLZ9814@orange.com>
2023-06-15 16:50:38 +0200

    remove config

--- code/test_aug.py
+++ code/test_aug.py
@@ -2,7 +2,6 @@ import os
 import cv2
 from utils import image_processor
 from utils import text_processor
-from utils.config import Config
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 
@@ -26,55 +25,34 @@ def process(input_dir, output_dir, pipeline):
     print("Finished.")
 
 
-def text_augment():
-    # Load configuration file
-    config = Config("config.json")
+# Get project file address
+project_dir = os.path.abspath(os.path.join(os.getcwd()))
 
-    # Define input and output directories
-    text_input_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                  config.raw_data_dir,
-                                  "annotations")
-    text_output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                   config.generated_data_dir,
-                                   "test2")
-
-    # Define text processing pipelines
-    text_pipeline = [
-        text_processor.ChangeCase(text_input_dir, text_output_dir)
-    ]
-
-    # Process text using the shared 'process' function
-    process(text_input_dir, text_output_dir, text_pipeline)
+# Define input and output directories
+image_data_dir = os.path.join(project_dir, "dataset", "raw_data", "images")
+image_save_dir = os.path.join(project_dir, "dataset", "generated_data", "test1")
+text_data_dir = os.path.join(project_dir, "dataset", "raw_data", "annotations")
+text_save_dir = os.path.join(project_dir, "dataset", "generated_data", "test2")
 
 
-def image_augment():
-    # Load configuration file
-    config = Config("config.json")
-
-    # Define input and output directories
-    image_input_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                   config.raw_data_dir,
-                                   "images")
-    image_output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                    config.generated_data_dir,
-                                    "test1")
-
-    # # test the code
-    # print(os.path.join(image_input_dir, "000.jpg"))
-    # image = cv2.imread(os.path.join(image_input_dir, "000.jpg"))
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # visualize(image)
-
-    # Define image and text processing pipelines
-    image_pipeline = [
-        # image_processor.Rotate(image_input_dir, image_output_dir)
-        # image_processor.ShiftScaleRotate(image_input_dir, image_output_dir)
-        image_processor.Flip(image_input_dir, image_output_dir)
-        # image_processor.StyleTransfer(image_input_dir, image_output_dir, model_path=config.model_path)
-    ]
-
-    # Process images using the shared 'process' function
-    process(image_input_dir, image_output_dir, image_pipeline)
+# test the code
+print(os.path.join(image_data_dir, "000.jpg"))
+image = cv2.imread(os.path.join(image_data_dir, "000.jpg"))
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+visualize(image)
 
 
-image_augment()
+# Define image and text processing pipelines
+image_pipeline = [
+    # image_processor.Rotate(image_input_dir, image_output_dir)
+    # image_processor.ShiftScaleRotate(image_input_dir, image_output_dir)
+    image_processor.Flip(image_data_dir, image_save_dir)
+    # image_processor.StyleTransfer(image_input_dir, image_output_dir, model_path=config.model_path)
+]
+
+text_pipeline = [
+    text_processor.ChangeCase(text_data_dir, text_save_dir)
+]
+
+process(image_data_dir, image_save_dir, image_pipeline)
+process(text_data_dir, text_save_dir, text_pipeline)
