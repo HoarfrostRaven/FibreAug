31d10afff96cda5b406c47ea581c9b64ae28efb3
XIANZHI Ye DTSI/SI <CZLZ9814@orange.com>
2023-06-23 09:56:28 +0200

    prepare dataset

--- code/preprocess.py
+++ code/preprocess.py
@@ -3,15 +3,15 @@ import os
 from PIL import Image
 
 folder_path = "C:\\Applications\\Projets\\FibreAug\\dataset\\raw_data\\images"
-save_path = "C:\\Applications\\Projets\\FibreAug\\dataset\\raw_data\\images.npy"
+save_path = "C:\\Applications\\Projets\\FibreAug\\dataset\\raw_data\\images_1024_768.npy"
 
 image_data = []
 
 for filename in os.listdir(folder_path):
-    if filename.endswith(".jpg") or filename.endswith(".JPG"):
+    if filename.endswith(".jpg"):
         image_path = os.path.join(folder_path, filename)
         image = Image.open(image_path)
-        image = image.resize((756, 1008))
+        image = image.resize((1024, 768)) # width * height
         image_array = np.array(image)
         image_data.append(image_array)
 
