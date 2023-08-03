88cc35cc13a1237ba7a2e9a369a4880967700f6c
XIANZHI Ye DTSI/SI <CZLZ9814@orange.com>
2023-08-03 10:40:33 +0200

    create new dataset

--- code/preprocess.py
+++ code/preprocess.py
@@ -2,8 +2,8 @@ import numpy as np
 import os
 from PIL import Image
 
-folder_path = "C:\\Applications\\Projets\\FibreAug\\dataset\\raw_data\\images"
-save_path = "C:\\Applications\\Projets\\FibreAug\\dataset\\raw_data\\images_1024_768.npy"
+folder_path = "C:\\Applications\\Projets\\FibreAug\\dataset\\raw_data\\fibres"
+save_path = "C:\\Applications\\Projets\\FibreAug\\dataset\\raw_data\\images_64_64.npy"
 
 image_data = []
 
@@ -11,7 +11,7 @@ for filename in os.listdir(folder_path):
     if filename.endswith(".jpg"):
         image_path = os.path.join(folder_path, filename)
         image = Image.open(image_path)
-        image = image.resize((1024, 768)) # width * height
+        image = image.resize((64, 64)) # width * height
         # Convert to numpy array
         image_array = np.array(image)
         image_data.append(image_array)
